1f44769fe98dd81c66bcb30bfe3b996d2957f950
luftboud <ijamaharyta@gmail.com>
2023-12-22 17:06:50 +0200

    //

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
   );
 };
